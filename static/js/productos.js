046df5fc9f938686e81fe4a4affefbf9edc05105
Rocío Catalano <rociocatalano4@hotmail.com>
2023-08-29 21:11:43 -0300

    agregando archivos de javascript y modificando algunos errores

---
+++ static/js/productos.js
@@ -0,0 +1,21 @@
+// Datos de ejemplo de productos
+
+const productos = [
+    { id: 1, nombre: 'Producto 1', precio: 10.99 },
+    { id: 2, nombre: 'Producto 2', precio: 20.99 },
+    { id: 3, nombre: 'Producto 3', precio: 15.99 }
+];
+
+// Función para mostrar la lista de productos
+function mostrarProductos() {
+    const lista = document.getElementById('productos-lista');
+
+    productos.forEach(producto => {
+        const li = document.createElement('li');
+        li.innerHTML = `<a href="producto.html?id=${producto.id}">${producto.nombre}</a> - $${producto.precio}`;
+        lista.appendChild(li);
+    });
+}
+
+// Mostrar la lista de productos cando la página haya cargado 
+window.addEventListener('load', mostrarProductos);
