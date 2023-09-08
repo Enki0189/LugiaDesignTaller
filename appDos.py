7d20a847ba4269e2a556031255fcc1860fa4c9d0
Mara Diaz <marudiaz0189@gmail.com>
2023-09-07 22:34:05 -0300

    Agrego metodos para abm de usuario.

---
+++ appDos.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template , request , redirect , url_for, jsonify
+from flask_mysqldb import MySQL
+
+#flask instance
+app = Flask(__name__)
+
+#configuracion base de datos
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '010420'
+app.config['MYSQL_DB'] = 'lugia_design'
+
+mysql = MySQL(app)
+
+
+#create route decorator
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/contacto.html')
+def contacto():
+    return render_template("contacto.html")
+
+@app.route('/nosotros.html')
+def nosotros():
+    return render_template("nosotros.html")
+
+#todos los productos
+@app.route('/productos.html')
+def productos():
+    return render_template("productos.html")
+
+#producto individual
+@app.route('/producto.html')
+def producto():
+    return render_template("producto.html")
+
+@app.route('/pagUsuario.html')
+def pagUsuario():
+    return render_template("pagUsuario.html")
+
+# rutas de usuario
+
+@app.route('/usuario' , methods = ['POST'])
+def crearUsuario():
+    print('Se recibe solicitud de creacion de nuevo usuario.')
+    id = 1
+    #nombreUsuario = request.form['nombreUsuario']
+    #contrasena = request.form['contrasena']
+    #nombreYapellido = request.form['nombreYapellido']
+    #email = request.form['email']
+    #telefono = request.form['telefono']
+    #direccion = request.form['direccion']
+    #provincia = request.form['provincia']
+    #cuil = request.form['cuil']
+    nombreUsuario = 'nombreUsuario'
+    contrasena = 'contrasena'
+    nombreYapellido = 'nombreYapellido'
+    email = 'email'
+    telefono = 1234567
+    direccion = 'direccion'
+    provincia = 'provincia'
+    cuil = 'cuil'
+    rol = 1
+    cur = mysql.connection.cursor()
+    print('Dando de alta usuario.')
+    cur.execute('INSERT INTO usuario (idUsuario, nombreUsuario, contraseña, rol, email, direccion, telefono, nombreYapellido, cuil, provincia) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (id, nombreUsuario, contrasena, rol, email, direccion, telefono, nombreYapellido, cuil, provincia))
+    mysql.connection.commit()
+    print('Usuario dado de alta.')
+
+@app.route('/usuario/<int:id>', methods=['DELETE'])
+def eliminarUsuario(id):
+    try:
+        cur = mysql.connection.cursor()
+
+        # Verifica si el usuario existe
+        cur.execute("SELECT * FROM usuario WHERE idUsuario = %s", (id,))
+        usuario = cur.fetchone()
+        if not usuario:
+            return "Usuario no encontrado", 404
+
+        # Elimina al usuario
+        cur.execute("DELETE FROM usuario WHERE idUsuario = %s", (id,))
+        mysql.connection.commit()
+
+        return "Usuario eliminado con éxito", 200
+
+    except Exception as e:
+        return f"Error al eliminar el usuario: {str(e)}", 500
+    
+
+
+@app.route('/usuarios', methods=['GET'])
+def obtenerUsuarios():
+    try:
+        cur = mysql.connection.cursor()
+        
+        # Consulta SQL para obtener todos los usuarios
+        cur.execute("SELECT * FROM usuario")
+        usuarios = cur.fetchall()
+        
+        # Convertir el resultado a una lista de diccionarios para JSONificar
+        lista_usuarios = []
+        for usuario in usuarios:
+            lista_usuarios.append({
+                'idUsuario': usuario[0],
+                'nombreUsuario': usuario[1],
+                'contraseña': usuario[2],
+                'rol': usuario[3],
+                'email': usuario[4],
+                'direccion': usuario[5],
+                'telefono': usuario[6],
+                'nombreYapellido': usuario[7],
+                'cuil': usuario[8],
+                'provincia': usuario[9]
+            })
+
+        # Devolver la lista de usuarios como JSON
+        return jsonify(lista_usuarios)
+
+    except Exception as e:
+        return f"Error al obtener los usuarios: {str(e)}", 500
+
+
+
+
+#no borrar por ahora
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
