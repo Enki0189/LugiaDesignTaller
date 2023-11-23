951ccb226a4514dc56e4e4c4d0c1bf590ee1d906
Mara Diaz <marudiaz0189@gmail.com>
2023-11-22 21:35:49 -0300

    se eliminan templates innecesarios

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.secret_key = 'alguna_clave_secreta_y_dificil_de_adivinar'
 #configuracion base de datos
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '****'
+app.config['MYSQL_PASSWORD'] = '010420'
 app.config['MYSQL_DB'] = 'lugia_design'
 
 mysql = MySQL(app)
